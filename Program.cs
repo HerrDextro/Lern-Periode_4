e98d2fc982a574fa43795be0de8f96b379bf0bae
Neoow <142605719+HerrDextro@users.noreply.github.com>
2024-03-01 17:59:06 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+class Public //nachher Tokenizer nach zusammenfügen mit Abbas code?
+{
+    public static List<string> Tokenize(string text)
+    {
+        MatchCollection matches = Regex.Matches(text, @"\b\w+\b");
+
+        List<string> words = new List<string>();
+        foreach (Match match in matches)
+        {
+            words.Add(match.Value);
+        }
+
+        return words;
+    }
+
+ 
+    static void Main()
+    {
+        string sentence = Console.ReadLine();
+        List<string> tokens = Tokenize(sentence);
+        Console.WriteLine("Tokens:");
+        foreach (string token in tokens)
+        {
+            Console.WriteLine(token);
+        }
+    }
+}
